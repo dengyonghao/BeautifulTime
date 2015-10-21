9eb37c9aceeb1415f39bd7c49dc141066e5fb6b6
dengyonghao <1179132021@qq.com>
2015-10-21 20:20:34 +0800

    coredatahelper

--- BeatuifulTime/AppDelegate.h
+++ BeatuifulTime/AppDelegate.h
@@ -8,19 +8,13 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
+#import "BTCoreDataHelper.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-//AppDelegate 实例
-+ (AppDelegate *) getInstance;
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
+@property (nonatomic, strong, readonly) BTCoreDataHelper *coreDataHelper;
 
 //进入新手引导页
 - (void)enterGuidePage;
