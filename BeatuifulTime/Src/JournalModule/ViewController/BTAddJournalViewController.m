d7177554d3d4c3bea174631596c8a02063cba0b1
dengyonghao <1179132021@qq.com>
2015-10-29 16:44:55 +0800

    回家前更新一把

--- BeatuifulTime/Src/JournalModule/ViewController/BTAddJournalViewController.m
+++ BeatuifulTime/Src/JournalModule/ViewController/BTAddJournalViewController.m
@@ -10,28 +10,27 @@
 
 @interface BTAddJournalViewController ()
 
+@property (nonatomic, strong) UIButton *finshBnt;
+@property (nonatomic, strong) UIScrollView *mainScrollView;
+@property (nonatomic, strong) UIButton *city;
+@property (nonatomic, strong) UIButton *weath;
+@property (nonatomic, strong) UIButton *phonos;
+@property (nonatomic, strong) UIButton *record;
+
 @end
 
 @implementation BTAddJournalViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.titleLabel.text = @"写日记";
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
