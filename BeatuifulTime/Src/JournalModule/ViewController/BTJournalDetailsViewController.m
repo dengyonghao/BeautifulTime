f40022b7f34190479dddf18b1750ee1ec644e1d8
dyh <cbyniypeu@163.com>
2015-12-09 23:50:00 +0800

    addressbook

---
+++ BeatuifulTime/Src/JournalModule/ViewController/BTJournalDetailsViewController.m
@@ -0,0 +1,34 @@
+//
+//  BTJournalDetailsViewController.m
+//  BeatuifulTime
+//
+//  Created by deng on 15/12/9.
+//  Copyright © 2015年 dengyonghao. All rights reserved.
+//
+
+#import "BTJournalDetailsViewController.h"
+
+@interface BTJournalDetailsViewController ()
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+@end
+
+@implementation BTJournalDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.titleLabel.text = @"详情";
+    [self.finishButton setHidden:NO];
+    [self.finishButton setTitle:@"编辑" forState:UIControlStateNormal];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)finishButtonClick {
+
+}
+
+@end
---
+++ BeatuifulTime/Src/SettingModule/View/BTAddressBookTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  BTAddressBookTableViewCell.h
+//  BeatuifulTime
+//
+//  Created by deng on 15/12/9.
+//  Copyright © 2015年 dengyonghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef void (^pullButtonClicked)(NSIndexPath * indexPath);
+
+@interface BTAddressBookTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) UILabel * nameLabel;
+@property (nonatomic, strong) UILabel * telLabel;
+@property (nonatomic, strong) UIButton * pullButton;//三角按钮
+@property (nonatomic, copy) pullButtonClicked pullBlock;
+@property (nonatomic, strong) UIView * line;
+@property (nonatomic, strong) NSIndexPath * indexPath;
+
+@end
