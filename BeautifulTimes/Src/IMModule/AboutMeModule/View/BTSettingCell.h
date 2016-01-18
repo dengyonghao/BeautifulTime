08ac0b9069877735247de9d11fa1d75080eddfcd
dengyonghao <1179132021@qq.com>
2016-01-18 20:21:58 +0800

    im ui

---
+++ BeautifulTimes/Src/IMModule/AboutMeModule/View/BTSettingCell.h
@@ -0,0 +1,13 @@
+//
+//  BTSettingCell.h
+//  BeautifulTimes
+//
+//  Created by dengyonghao on 16/1/18.
+//  Copyright © 2016年 dengyonghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTSettingCell : UITableViewCell
+
+@end
